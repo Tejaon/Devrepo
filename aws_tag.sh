4218586c11599b2d55f3b54c637a15fdb774c7ba
Teja T <tejaon@yahoo.com>
2019-09-21 10:09:04 +0000

    second aws file

--- aws_tag.sh
+++ aws_tag.sh
@@ -1,1 +1,1 @@
-aws ec2 create-tags  --resources ami-00c03f7f7f2ec15c3 --tags Key=NEW,Value=production --region us-east-2
+aws ec2 create-tags  --resources ami-00c03f7f7f2ec15c3  i-018f45f0124d62cac --tags Key=NEW,Value=production Key=NEW,Value=production2  --region us-east-2
