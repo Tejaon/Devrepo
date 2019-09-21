fa58d5395e0a6dd84d46ed24166834d79ebf9293
Teja T <tejaon@yahoo.com>
2019-09-21 09:14:40 +0000

    first aws file

--- aws_cli.sh
+++ aws_cli.sh
@@ -1,2 +1,3 @@
 aws ec2 run-instances --instance-type t2.micro --count 1 --key-name putty_pr.ppk --subnet-id subnet-04ffb7d80dea311ca --image-id ami-00c03f7f7f2ec15c3  --security-group-ids sg-03f92ee69f02a5fc3
+echo "Jenkins instace created"
 
