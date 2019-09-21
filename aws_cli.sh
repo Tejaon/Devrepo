74572bf519b2ab8c2a5945ad1724f0db92afcf19
Teja T <tejaon@yahoo.com>
2019-09-21 09:08:11 +0000

    first aws file

---
+++ aws_cli.sh
@@ -0,0 +1,2 @@
+aws ec2 run-instances --instance-type t2.micro --count 1 --key-name putty_pr.ppk --subnet-id subnet-04ffb7d80dea311ca --image-id ami-00c03f7f7f2ec15c3  --security-group-ids sg-03f92ee69f02a5fc3
+
