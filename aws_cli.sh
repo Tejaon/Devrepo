cb4053d4c6fff5213b61b9a1dba17987a8ecca0f
Teja T <tejaon@yahoo.com>
2019-09-29 08:32:42 +0000

    first

--- aws_cli.sh
+++ aws_cli.sh
@@ -1,4 +1,4 @@
-echo "AWS instance "
+echo "AWS instance on slave1 "
 aws ec2 run-instances --instance-type t2.micro --count 1 --key-name putty_pr.ppk --subnet-id subnet-04ffb7d80dea311ca --image-id ami-00c03f7f7f2ec15c3  --security-group-ids sg-03f92ee69f02a5fc3 --region us-east-2
 echo "Jenkins instace created"
 
